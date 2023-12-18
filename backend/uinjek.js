7a86b16ca1474dbcc7e65227440a7f07cc17798d
arbaap <arbaadhipamungkas@gmail.com>
2023-12-18 14:29:44 +0700

    deploy

--- backend/uinjek.js
+++ backend/uinjek.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+mongoose.set("strictQuery", false);
+
+var mongoURL =
+  "mongodb+srv://uinjek:uinjek@cluster0.93sie2d.mongodb.net/db-uinjek";
+
+mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true });
+
+var connection = mongoose.connection;
+
+connection.on("error", () => {
+  console.log("Mongo DB Connection Failed");
+});
+
+connection.on("connected", () => {
+  console.log("Mongo DB Connection Success");
+});
 
 const app = express();
 
-const dbConfig = require("./dbmongose");
+app.use(cors());
+
+app.use(express.json());
 
 const adminsRoute = require("./routes/adminsRoute");
 const driversRoute = require("./routes/driversRoute");
@@ -10,7 +31,12 @@ const pelanggansRoute = require("./routes/pelanggansRoute");
 const ordersRoute = require("./routes/ordersRoutes");
 const testRoute = require("./routes/testRoute");
 
-app.use(express.json());
+app.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
+});
 
 app.use("/api/admins", adminsRoute);
 app.use("/api/drivers", driversRoute);
@@ -18,6 +44,8 @@ app.use("/api/pelanggans", pelanggansRoute);
 app.use("/api/orders", ordersRoute);
 app.use("/api/test", testRoute);
 
-const port = process.env.PORT || 5000;
+app.listen(3000, () => {
+  console.log("Server uinjek is running on port 3000");
+});
 
-app.listen(port, () => console.log("Server UINJEK is running on port", port));
+module.exports = app;
